0f76bf0bf7bc4238672a8f3a5c09d5d986f997d6
Nick <nickeast1998@gmail.com>
2021-05-21 12:13:29 +0100

    url resolver

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'gatsby';
 import styled from 'styled-components';
-import Layout from '../components/layout';
-import SEO from '../components/functional/seo';
 import Hero from '../components/sections/hero';
 import UVP from '../components/sections/uvp';
 import Services from '../components/sections/services';
 import WhoWeAre from '../components/sections/whoweare';
 import WhereWeWork from '../components/sections/wherewework';
+import Layout from '../components/layout';
 
 const IndexPageStyles = styled.section`
   width: 100%;
