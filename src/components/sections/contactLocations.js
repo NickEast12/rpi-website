ba241e7acf9ef285a5455767cbb07131503d4b80
Nick <nickeast1998@gmail.com>
2021-07-09 12:22:47 +0100

    contact boxes refactored into dynamic data pulled from sanity

--- src/components/sections/contactLocations.js
+++ src/components/sections/contactLocations.js
@@ -7,6 +7,7 @@ import Pin from '../../svgs/pin.svg';
 
 const ContactLocationsStyles = styled.section`
   width: 100%;
+  padding-bottom: 2rem;
   .locations {
     max-width: var(--maxWidth);
     padding: 1.5rem 0 2rem 0;
@@ -19,13 +20,15 @@ const ContactLocationsStyles = styled.section`
     &__btns {
       display: flex;
       justify-content: center;
+      flex-wrap: wrap;
       align-items: center;
-      /* gap: 1rem; */
+      /* gap: 15px; */
       margin-top: 1.5rem;
       button {
         margin-right: 10px;
         cursor: pointer;
-        padding: 0.5rem 1rem;
+        padding: 0.5rem;
+        margin-bottom: 0.75rem;
         border: solid var(--background) 2px;
         border-radius: 50px;
         background-color: transparent;
@@ -56,282 +59,115 @@ const ContactLocationsStyles = styled.section`
   .offices {
     max-width: var(--maxWidth);
     margin: var(--auto);
-    @media only screen and (min-width: 1024px) {
-      display: grid;
-      grid-template-columns: 60% 1fr;
-      grid-column-gap: 1rem;
-      &__box {
-        height: 100%;
-        &__wrapper {
-          display: grid;
-          grid-template-columns: repeat(2, 1fr);
-        }
-        &:nth-child(2) {
-          .offices__box__wrapper {
-            grid-template-columns: 1fr;
-          }
-        }
-      }
-    }
+    width: 90%;
     &__box {
-      margin-top: 2rem;
-      max-width: var(--maxWidth);
+      padding: 1rem 0;
+      margin-bottom: 0.5rem;
       h4 {
         text-align: center;
-        margin-bottom: 2rem;
-        @media only screen and (min-width: 1024px) {
-          text-align: left;
-        }
       }
-      &__wrapper {
+      section {
+        display: grid;
+        grid-gap: 1rem;
+        margin: 2rem auto 0 auto;
+        max-width: 700px;
+      }
+      &:nth-child(1) {
         section {
-          &:nth-child(1n) {
-            border-bottom: solid 8px var(--newBlue);
-            @media only screen and (min-width: 1024px) {
-              border-bottom: none;
-            }
-          }
-          &:nth-child(2n) {
-            border-bottom: solid 8px var(--newCyan);
-            @media only screen and (min-width: 1024px) {
-              border-bottom: none;
-            }
-          }
-          border-radius: 2.5px;
-          padding: 1.5rem 3rem;
-          display: flex;
-          align-items: center;
-          flex-direction: column;
-          text-align: center;
-          justify-content: center;
-          svg {
-            width: 55px;
-            margin-bottom: 1rem;
-          }
-          h5 {
-            font-weight: 700;
-            font-size: 1.875rem;
-            line-height: 2.25rem;
-            letter-spacing: -1.5px;
-            margin-bottom: 0.5rem;
-          }
-          address {
-            margin-bottom: 0.5rem;
-            margin: 0.5rem auto;
-            max-width: 400px;
-          }
-          p {
-            font-weight: 700;
-            margin-bottom: 0.25rem;
+          @media only screen and (min-width: 768px) {
+            grid-template-columns: repeat(2, 1fr);
           }
         }
       }
-      @media only screen and (min-width: 1024px) {
-        .europe,
-        .dubai,
-        .apac {
-          grid-gap: 1rem;
-          section {
-            height: 100%;
-            padding: 1rem 1.5rem;
-            justify-content: flex-start;
-            align-items: flex-start;
-            box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
-            address {
-              text-align: left;
-            }
-            &:nth-child(odd) {
-              border-top: solid 8px var(--newBlue);
-            }
-            &:nth-child(even) {
-              border-top: solid 8px var(--newCyan);
-            }
-          }
-        }
-        .apac {
-          grid-template-columns: repeat(4, 1fr);
+      &:nth-child(2) {
+        section {
+          grid-template-columns: repeat(1, 1fr);
         }
       }
-    }
-  }
-  .alt-o {
-    @media only screen and (min-width: 1024px) {
-      grid-template-columns: repeat(1, 1fr);
-      padding-bottom: 3rem;
+      &:nth-child(3) {
+        section {
+          @media only screen and (min-width: 768px) {
+            grid-template-columns: repeat(2, 1fr);
+          }
+        }
+      }
+      &:nth-child(4) {
+        section {
+          max-width: 1100px;
+          @media only screen and (min-width: 768px) {
+            grid-template-columns: repeat(3, 1fr);
+          }
+        }
+      }
     }
   }
 `;
-const ContactLocations = () => {
-  const i = true;
+const ContactLocations = ({ regions, offices }) => {
+  const [europe, middleEast, asia, pacific, america] = offices;
+  const europeData = europe.nodes;
+  const middleEastAfrica = middleEast.nodes;
+  const pacificData = pacific.nodes;
+  const asiaData = asia.nodes;
+  const americaData = america.nodes;
+  console.log(asiaData);
   return (
     <ContactLocationsStyles>
       <div className="locations">
         <h3>Our locations around the globe</h3>
         <div className="locations__btns">
-          <Link to="/contact#uk-europe">
-            <button type="button">
-              <span>UK &amp; Europe</span>
-            </button>
-          </Link>
-          <Link to="/contact#mea">
-            <button type="button">
-              <span>MEA</span>
-            </button>
-          </Link>
-          <Link to="/contact#apac">
-            <button type="button">
-              <span>APAC</span>
-            </button>
-          </Link>
+          {regions.map((button) => (
+            <Link
+              to={`/contact/#${button.slug.current}`}
+              key={button.slug.current}
+            >
+              <button type="button">
+                <span>{button.title}</span>
+              </button>
+            </Link>
+          ))}
         </div>
         <LocationMap />
       </div>
       <div className="offices">
-        <div className="offices__box" id="uk-europe">
+        <div className="offices__box" id="uk-and-europe">
           <h4>UK &amp; Europe</h4>
-          <div className="offices__box__wrapper europe">
-            <section>
-              <Pin />
-              <h5>London</h5>
-              <address>
-                <a
-                  href="https://www.google.com/maps/place/5th+Floor,+1+Poultry,+London+EC2R+8EJ/@51.5133463,-0.0927404,17z/data=!3m1!4b1!4m5!3m4!1s0x48760354e6a16a63:0x6ac3d4a9cbeaf534!8m2!3d51.5133463!4d-0.0905464"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  5th Floot, 1 Poultry, London, EC2R 8EJ, UK
-                </a>
-              </address>
-              <p>
-                <a href="tel:+44 (0) 203 597 7150"> + 44 (0) 203 597 7150</a>
-              </p>
-              <p>
-                <a href="mailto:people@rpint.com"> people@rpint.com</a>
-              </p>
-            </section>
-            <section>
-              <Pin />
-              <h5>Stuttgart</h5>
-              <address>
-                <a
-                  href="https://www.google.com/maps/place/Naststra%C3%9Fe+15b,+70376+Stuttgart,+Germany/@48.8144932,9.2115734,17z/data=!3m1!4b1!4m5!3m4!1s0x4799c54755e6aaab:0x6afce6e401d85160!8m2!3d48.8144932!4d9.2137674"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Naststrasse 15B Stuttgart 70376 Germany
-                </a>
-              </address>
-              <p>
-                <a href="tel:+49 711 214756 40"> + 49 711 214756 40</a>
-              </p>
-              <p>
-                <a href="mailto:germany@rpint.com"> germany@rpint.com</a>
-              </p>
-            </section>
-          </div>
+          <section>
+            {europeData.map((office) => (
+              <SingleOffice key={office.id} office={office} />
+            ))}
+          </section>
         </div>
-        <div className="offices__box" id="mea">
-          <h4>MEA</h4>
-          <div className="offices__box__wrapper dubai">
-            <section>
-              <Pin />
-              <h5>Dubai</h5>
-              <address>
-                <a
-                  href="https://www.google.com/maps/place/Dubai+Outsource+City/@25.1237489,55.4231638,17z/data=!4m8!1m2!2m1!1sOffice+EX-41+Ground+Floor,+Building+7+Dubai+Outsource+City+UAE+++++++++++++++++(PO+Box+500313)!3m4!1s0x3e5f63bd5e70ad87:0x4feb6ba83055a868!8m2!3d25.1239098!4d55.4252854"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Office EX-41 Ground Floor, Building 7 Dubai Outsource City UAE
-                  (PO Box 500313)
-                </a>
-              </address>
-              <p>
-                <a href="tel:971 (0)4 394 8760" rel="noopener noreferrer">
-                  + 971 (0)4 394 8760
-                </a>
-              </p>
-              <p>
-                <a href="mailto:dubai@rpint.com"> dubai@rpint.com</a>
-              </p>
-            </section>
-          </div>
+        <div className="offices__box" id="middle-east-and-africa">
+          <h4>Middle East &amp; Africa</h4>
+          <section>
+            {middleEastAfrica.map((office) => (
+              <SingleOffice key={office.id} office={office} />
+            ))}
+          </section>
         </div>
-      </div>
-      <div className="offices alt-o">
-        <div className="offices__box" id="apac">
-          <h4>APAC</h4>
-          <div className="offices__box__wrapper apac">
-            <section>
-              <Pin />
-              <h5>Singapore</h5>
-              <address>
-                <a
-                  href="https://www.google.com/maps/place/One+Marina+Boulevard+Office+Building/@1.2822004,103.8504573,17z/data=!3m1!4b1!4m5!3m4!1s0x31da19764d461df5:0x1c75e38f90b41fbd!8m2!3d1.2822004!4d103.8526513"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  One Marina Boulevard Lvl20 1 Marina Boulevard Singapore 018989
-                </a>
-              </address>
-              <p>
-                <a href="tel:+65 69785831">+ 65 69785831</a>
-              </p>
-              <p>
-                <a href="mailto:singapore@rpint.com"> singapore@rpint.com</a>
-              </p>
-            </section>
-            <section>
-              <Pin />
-              <h5>Malaysia</h5>
-              <address>
-                <a
-                  href="https://www.google.com/maps/place/TDCX+Malaysia+(NU+Tower+2+Office)/@3.1333054,101.6840409,17z/data=!3m1!4b1!4m5!3m4!1s0x31cc4992c3f7607b:0xa0d05a713540da90!8m2!3d3.1333054!4d101.6862349"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Level 23, NU Tower 2 Jalan Tun Sambanthan, 50470 KL Sentral
-                  Kuala Lumpur, Malaysia
-                </a>
-              </address>
-              <p>
-                <a href="tel:+(603) 2727 1751"> + (603) 2727 1751</a>
-              </p>
-              <p>
-                <a href="mailto:rp@rpint.com"> rp@rpint.com</a>
-              </p>
-            </section>
-            <section>
-              <Pin />
-              <h5>New Zealand</h5>
-              <address>
-                <a
-                  href="https://www.google.com/maps/place/BDH+Business+Hub/@-36.7472428,174.7344563,17z/data=!3m1!4b1!4m5!3m4!1s0x6d0d3a2bfd2d32f7:0xa99e3be5822083d8!8m2!3d-36.7472308!4d174.7365569"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  BDH Business Hub 4 Whetu Place Rosedale New Zealand 0630
-                </a>
-              </address>
-              <p>
-                <a href="tel:+64 (0) 9 9284031">+ 64 (0) 9 9284031</a>
-              </p>
-              <p>
-                <a href="mailto:anz@rpint.com">anz@rpint.com</a>
-              </p>
-            </section>
-            <section>
-              <Pin />
-              <h5>Hong Kong</h5>
-              <address>
-                Rooms 1007-1012, 10/F., K. Wah Centre 191 Java Road, North Point
-                Hong Kong
-              </address>
-              <p>
-                <a href="mailto:zs@rpint.com "> zs@rpint.com</a>
-              </p>
-            </section>
-          </div>
+        <div className="offices__box" id="anz-and-pacific">
+          <h4>ANZ &amp; Pacific</h4>
+          <section>
+            {pacificData.map((office) => (
+              <SingleOffice key={office.id} office={office} />
+            ))}
+          </section>
+        </div>
+        <div className="offices__box" id="asia">
+          <h4>Asia</h4>
+          <section>
+            {asiaData.map((office) => (
+              <SingleOffice key={office.id} office={office} />
+            ))}
+          </section>
+        </div>
+        <div className="offices__box" id="americas">
+          <h4>Americas</h4>
+          <section>
+            {americaData.map((office) => (
+              <SingleOffice key={office.id} office={office} />
+            ))}
+          </section>
         </div>
       </div>
     </ContactLocationsStyles>
@@ -339,3 +175,76 @@ const ContactLocations = () => {
 };
 
 export default ContactLocations;
+
+const SingleOfficeStyles = styled.div`
+  width: 100%;
+  background-color: #fff;
+  box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+  border-top: solid 8px var(--newBlue);
+  border-radius: 10px;
+  margin: 0 auto 0.5rem auto;
+  padding: 1rem;
+  width: 90%;
+  max-width: 350px;
+  .s-o {
+    svg {
+      width: 40px;
+      height: 40px;
+    }
+    text-align: center;
+    h5 {
+      font-size: 1.5rem;
+      margin: 0.5rem 0;
+    }
+    address {
+      margin-bottom: 0.5rem;
+    }
+    &--number,
+    &--email,
+    &--ea {
+      font-weight: 700;
+    }
+    &--email {
+      margin-top: 0.25rem;
+    }
+  }
+`;
+const SingleOffice = ({ office }) => {
+  const location =
+    office.addressone +
+    office.addresstwo +
+    office.addressthree +
+    office.addressfour;
+  const t = location.split(' ');
+  return (
+    <SingleOfficeStyles>
+      <div className="s-o">
+        <Pin />
+        <h5>{office.name}</h5>
+        <address>
+          <a
+            href={`https://what3words.com/${office.whatthreewords}`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <p>{office.addressone}</p>
+            <p>{office.addresstwo}</p>
+            <p>{office.addressthree}</p>
+            <p>{office.addressfour}</p>
+          </a>
+        </address>
+        <a href={`tel:${office.number}`}>
+          <p className="s-o--number">{office.number}</p>
+        </a>
+        <a href={`mailto:${office.email}`}>
+          <p className="s-o--email">{office.email}</p>
+        </a>
+        {office.ealicence ? (
+          <p className="s-o--ea">{`EA Licence : ${office.ealicence}`}</p>
+        ) : (
+          ''
+        )}
+      </div>
+    </SingleOfficeStyles>
+  );
+};
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 //* Local imports
 import Layout from '../components/Layout';
 import CMain from '../components/sections/cMain';
@@ -7,8 +8,14 @@ import CTA from '../components/CTA';
 import DarkHeader from '../components/sections/darkHeader';
 import SEO from '../components/functional/SEO';
 
-const Contact = () => {
-  const i = true;
+const Contact = ({ data }) => {
+  const regions = data.RegionsQuery.nodes;
+  const pacific = data.PacificQuery;
+  const europe = data.EuropeQuery;
+  const middleEast = data.MiddleEastQuery;
+  const asia = data.AsiaQuery;
+  const america = data.AmericaQuery;
+  const offices = [europe, middleEast, asia, pacific, america];
   return (
     <Layout>
       <SEO
@@ -17,9 +24,148 @@ const Contact = () => {
       />
       <DarkHeader title="Are you ready to talk about talent that transforms your business? " />
       <CMain />
-      <ContactLocations />
+      <ContactLocations offices={offices} regions={regions} />
     </Layout>
   );
 };
 
 export default Contact;
+
+export const query = graphql`
+  query ContactQuery {
+    RegionsQuery: allSanityRegion {
+      nodes {
+        slug {
+          current
+        }
+        title
+        id
+      }
+    }
+    PacificQuery: allSanityLocations(
+      filter: {
+        regions: { elemMatch: { slug: { current: { eq: "anz-and-pacific" } } } }
+      }
+    ) {
+      nodes {
+        regions {
+          title
+          slug {
+            current
+          }
+        }
+        name
+        addressone
+        addresstwo
+        addressthree
+        addressfour
+        whatthreewords
+        ealicence
+        email
+        id
+        postcode
+        number
+      }
+    }
+    EuropeQuery: allSanityLocations(
+      filter: {
+        regions: { elemMatch: { slug: { current: { eq: "uk-and-europe" } } } }
+      }
+    ) {
+      nodes {
+        regions {
+          title
+          slug {
+            current
+          }
+        }
+        name
+        addressone
+        addresstwo
+        addressthree
+        addressfour
+        whatthreewords
+        ealicence
+        email
+        id
+        postcode
+        number
+      }
+    }
+    MiddleEastQuery: allSanityLocations(
+      filter: {
+        regions: {
+          elemMatch: { slug: { current: { eq: "middle-east-and-africa" } } }
+        }
+      }
+    ) {
+      nodes {
+        regions {
+          title
+          slug {
+            current
+          }
+        }
+        name
+        addressone
+        addresstwo
+        addressthree
+        addressfour
+        whatthreewords
+        ealicence
+        email
+        id
+        postcode
+        number
+      }
+    }
+    AsiaQuery: allSanityLocations(
+      filter: { regions: { elemMatch: { slug: { current: { eq: "asia" } } } } }
+    ) {
+      nodes {
+        regions {
+          title
+          slug {
+            current
+          }
+        }
+        name
+        addressone
+        addresstwo
+        addressthree
+        addressfour
+        whatthreewords
+        ealicence
+        email
+        id
+        postcode
+        number
+      }
+    }
+    AmericaQuery: allSanityLocations(
+      filter: {
+        regions: { elemMatch: { slug: { current: { eq: "americas" } } } }
+      }
+    ) {
+      nodes {
+        regions {
+          title
+          slug {
+            current
+          }
+        }
+        name
+        addressone
+        addresstwo
+        addressthree
+        addressfour
+        whatthreewords
+        ealicence
+        email
+        id
+        postcode
+        number
+      }
+    }
+  }
+`;
