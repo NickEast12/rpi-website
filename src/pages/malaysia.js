a97fee9fedfe8809fb401592e228d58d909a787d
Nick <nickeast1998@gmail.com>
2021-07-21 11:29:55 +0100

    holding under construction page

---
+++ src/pages/malaysia.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Layout from '../components/Layout';
+import Message from '../components/sections/message';
+
+const Malaysia = () => (
+  <Layout>
+    <Message
+      code="Page is under construction"
+      title="Please bear with us as we make changes to this page"
+      btn="Home"
+    />
+  </Layout>
+);
+
+export default Malaysia;
