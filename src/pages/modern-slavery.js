d4a8ad23a020f8e0e969188cd0a498187503ff99
Nick East <nickeast1998@gmail.com>
2023-08-29 20:53:21 +0100

    latest

--- src/pages/modern-slavery.js
+++ src/pages/modern-slavery.js
@@ -17,184 +17,61 @@ const ModernSlavery = () => {
       />
       <LegalHeader title="Modern Slavery" date="1st March 2021" />
       <LegalBody>
-        <section>
-          <p>
-            <span>RPI consists of the following operating companies: </span>
-          </p>
-          <p>
-            RP International Ltd registered in England, RP International
-            Resources (Europe) Ltd registered in England, RPI Consulting GmbH
-            registered in Germany, RP International FZ-LLC registered in the
-            UAE, RP International Labors Supply LLC registered in the UAE, RP
-            International Resources Pte Ltd registered in Singapore, RP
-            International Resources Ltd registered in Hong Kong, RP
-            International Resources Malaysia Sdn.Bhd registered in Malaysia, RP
-            International Resources Pty Ltd registered in Australia, RP
-            International Inc. registered in the USA and RP International New
-            Zealand Ltd registered in New Zealand. This group is described as
-            RPI in the rest of this document.
-          </p>
-          <p>
-            RPI is a global search and strategic recruitment firm, specialising
-            in the provision of leadership and technology professionals across
-            multiple specialisms and sectors, offering three core recruitment
-            services: Executive, Permanent and Interim &amp; Contract.
-          </p>
-          <p>
-            RPI take Anti-Slavery and Human Trafficking extremely seriously and
-            have made information available to you to explain:
-          </p>
-          <ul>
-            <li>About this policy</li>
-            <li>Scope of this policy</li>
-            <li>Responsibility of this policy</li>
-            <li>Compliance with this policy</li>
-            <li>Breach of this policy</li>
-            <li>How you can contact us</li>
-          </ul>
-        </section>
-        <section>
-          <h4>About this policy</h4>
-          <ul>
-            <li>
-              Modern slavery is a criminal offence under the Modern Slavery Act
-              2015 (the “Act”). It can occur in various forms, including
-              servitude, forced or compulsory labor and human trafficking, all
-              of which have in common the deprivation of a person’s liberty by
-              another to exploit them for personal or commercial gain.
-            </li>
-            <li>
-              This policy sets out RPI’s actions to understand all potential
-              modern slavery risks related to our business and to put in place
-              steps that are aimed at ensuring that there is no slavery or human
-              trafficking throughout our business or any of its supply chains.
-            </li>
-          </ul>
-        </section>
-        <section>
-          <h4>Scope of this policy</h4>
-          <ul>
-            <li>
-              RPI have a zero-tolerance approach to modern slavery and is
-              absolutely committed to preventing slavery and human trafficking
-              in any of its corporate activities and to ensure its supply chains
-              are free from slavery and human trafficking. We are committed to
-              acting ethically and with integrity in all our business dealings,
-              relationships and to implementing and enforcing effective systems
-              and controls to ensure modern slavery is not taking place anywhere
-              in our business.
-            </li>
-            <li>
-              We are committed to ensuring there is transparency in our business
-              and in our approach to tacking modern slavery throughout our
-              organization, consistent with our disclosure obligations under the
-              Modern Slavery Act 2015.
-            </li>
-            <li>
-              This policy applies to everyone working for us or on our behalf in
-              any capacity, including employees at all levels, contractors,
-              external contractors, interns, volunteers, third-party
-              representatives, candidates, clients and suppliers.
-            </li>
-            <li>
-              This policy does not form part of employees’ contract of
-              employment and may be amended it at any time.
-            </li>
-          </ul>
-        </section>
-        <section>
-          <h4>Responsibility of this policy</h4>
-          <ul>
-            <li>
-              The board of directors has overall responsibility for ensuring
-              this policy complies with our legal and ethical obligations, and
-              that those under our control comply with it.
-            </li>
-            <li>
-              The People team has primary responsibilities for implementing this
-              policy, monitoring its use and effectiveness, dealing with any
-              queries about it and auditing internal control processes to ensure
-              they are effective in countering modern slavery.
-            </li>
-            <li>
-              Line managers at all levels are responsible for ensuring those
-              reporting to them understand and comply with this policy and are
-              given adequate and regular training on it and the issue of modern
-              slavery in supply chains.
-            </li>
-            <li>
-              Everyone working with or for RPI must ensure that they read,
-              understand and comply with this policy.
-            </li>
-          </ul>
-        </section>
-        <section>
-          <h4>Compliance with this policy</h4>
-          <ul>
-            <li>
-              The responsibility of prevention, detection and reporting of
-              modern slavery in any part of our business lies with everyone
-              working for us or under our control. You are required to avoid any
-              activity that might lead to, or suggest, a breach of this policy.
-            </li>
-            <li>
-              You must notify our People team as soon as possible if you believe
-              or suspect that a conflict with this policy has occurred or may
-              occur in the future.
-            </li>
-            <li>
-              You are encouraged to raise concerns about any issue or suspicion
-              of modern slavery in any parts of our business or supply chains of
-              any supplier tier at the earliest possible stage.
-            </li>
-            <li>
-              If you believe or suspect a breach of this policy has occurred or
-              that it may occur, you must report it in accordance with our
-              Whistleblowing Policy as soon as possible.
-            </li>
-            <li>
-              If you are unsure about whether a particular act, the treatment of
-              workers more generally, or their working conditions within any
-              tier of our supply chains constitutes any of the various forms of
-              modern slavery, raise it with your line manager.
-            </li>
-            <li>
-              We aim to encourage openness and will support anyone who raises
-              genuine concerns in good faith under this policy, even if they
-              turn out to be mistaken. We are committed to ensuring no one
-              suffers any detrimental treatment as a result of reporting in good
-              faith their suspicion that modern slavery of whatever form is or
-              may be taking place in any part of our own business or in any of
-              our supply chains.
-            </li>
-          </ul>
-        </section>
-        <section>
-          <h4>Breach of this policy</h4>
-          <ul>
-            <li>
-              Any employee who breaches this policy will face disciplinary
-              action, which could result in dismissal for misconduct or gross
-              misconduct.
-            </li>
-            <li>
-              We may terminate our relationship with other individuals and
-              organisations working on our behalf if they breach this policy.
-            </li>
-          </ul>
-        </section>
-        <section>
-          <h4>How you can contact us</h4>
-          <p>
-            This Modern Slavery and Human Trafficking policy is reviewed
-            annually unless there is a requirement to review it more frequently.
-          </p>
-          <p>
-            Please contact us on{' '}
-            <a href="mailto:people@rpint.com">people@rpint.com</a> if you have
-            any further questions.
-          </p>
-        </section>
+      <section>
+      <p><strong>Modern Slavery and Child Labour Policy (collectively referred to as Modern Slavery)</strong></p>
+<p><strong>RPI consists of the following operating companies:</strong></p>
+<p>RP International Ltd registered in England, RP International Resources (Europe) Ltd registered in England, RPI Consulting GmbH registered in Germany, RP International FZ-LLC registered in the UAE, RP International Labors Supply LLC registered in the UAE, RP International Resources Pte Ltd registered in Singapore, RP International Resources Ltd registered in Hong Kong, RP International Resources Malaysia Sdn.Bhd registered in Malaysia, RP International Resources Pty Ltd registered in Australia, RP International Inc. registered in the USA and RP International New Zealand Ltd registered in New Zealand. This group is described as RPI in the rest of this document.</p>
+<p>RPI is a global search and strategic recruitment firm, specialising in the provision of leadership and technology professionals across multiple specialisms and sectors, offering three core recruitment services: Executive, Permanent and Interim &amp; Contract.</p>
+<p>RPI take Anti-Slavery, Human Trafficking and Child Labour extremely seriously and have made information available to you to explain:</p>
+<ul>
+<li>About this policy</li>
+<li>Scope of this policy</li>
+<li>Responsibility of this policy</li>
+<li>Compliance with this policy</li>
+<li>Breach of this policy</li>
+<li>How you can contact us</li>
+</ul>
+<p><strong>About this policy</strong></p>
+<ul>
+<li>Modern slavery is a criminal offence under the Modern Slavery Act 2015 (the &ldquo;Act&rdquo;). It can occur in various forms, including servitude, forced or compulsory labor and human trafficking, all of which have in common the deprivation of a person&rsquo;s liberty by another to exploit them for personal or commercial gain.</li>
+<li>This policy sets out RPI&rsquo;s actions to understand all potential modern slavery risks related to our business and to put in place steps that are aimed at ensuring that there is no slavery or human trafficking throughout our business or any of its supply chains.</li>
+<li>At no time will any company in the RPI Group of companies be involved in the sourcing, recruitment, presenting or employing directly of any minor for the purposes of gaining work.</li>
+<li>We will ensure that any party in our supply chain including suppliers, vendors and contractors also follows the relevant laws and cares for children&rsquo;s interests at all times.</li>
+<li>As an organization, we want to do business in a legal, ethical manner adding value to society and the environment, not causing harm.</li>
+<li>U.N Convention on the Rights of the Child will be followed at all times.</li>
+</ul>
+<p><strong>Scope of this policy</strong></p>
+<ul>
+<li>RPI have a zero-tolerance approach to modern slavery and is absolutely committed to preventing slavery and human trafficking in any of its corporate activities and to ensure its supply chains are free from slavery and human trafficking. We are committed to acting ethically and with integrity in all our business dealings, relationships and to implementing and enforcing effective systems and controls to ensure modern slavery is not taking place anywhere in our business.</li>
+<li>We are committed to ensuring there is transparency in our business and in our approach to tacking modern slavery throughout our organization, consistent with our disclosure obligations under the Modern Slavery Act 2015.</li>
+<li>This policy applies to everyone working for us or on our behalf in any capacity, including employees at all levels, contractors, external contractors, interns, volunteers, third-party representatives, candidates, clients and suppliers.</li>
+<li>This policy does not form part of employees&rsquo; contract of employment and may be amended it at any time.</li>
+</ul>
+<p><strong>Responsibility of this policy</strong></p>
+<ul>
+<li>The board of directors has overall responsibility for ensuring this policy complies with our legal and ethical obligations, and that those under our control comply with it.</li>
+<li>The People team has primary responsibilities for implementing this policy, monitoring its use and effectiveness, dealing with any queries about it and auditing internal control processes to ensure they are effective in countering modern slavery.</li>
+<li>Line managers at all levels are responsible for ensuring those reporting to them understand and comply with this policy and are given adequate and regular training on it and the issue of modern slavery in supply chains.</li>
+<li>Everyone working with or for RPI must ensure that they read, understand and comply with this policy.</li>
+</ul>
+<p><strong>Compliance with this policy</strong></p>
+<ul>
+<li>The responsibility of prevention, detection and reporting of modern slavery in any part of our business lies with everyone working for us or under our control. You are required to avoid any activity that might lead to, or suggest, a breach of this policy.</li>
+<li>You must notify our People team as soon as possible if you believe or suspect that a conflict with this policy has occurred or may occur in the future.</li>
+<li>You are encouraged to raise concerns about any issue or suspicion of modern slavery in any parts of our business or supply chains of any supplier tier at the earliest possible stage.</li>
+<li>If you believe or suspect a breach of this policy has occurred or that it may occur, you must report it in accordance with our Whistleblowing Policy as soon as possible.</li>
+<li>If you are unsure about whether a particular act, the treatment of workers more generally, or their working conditions within any tier of our supply chains constitutes any of the various forms of modern slavery, raise it with your line manager.</li>
+<li>We aim to encourage openness and will support anyone who raises genuine concerns in good faith under this policy, even if they turn out to be mistaken. We are committed to ensuring no one suffers any detrimental treatment as a result of reporting in good faith their suspicion that modern slavery of whatever form is or may be taking place in any part of our own business or in any of our supply chains.</li>
+</ul>
+<p><strong>Breach of this policy</strong></p>
+<ul>
+<li>Any employee who breaches this policy will face disciplinary action, which could result in dismissal for misconduct or gross misconduct.</li>
+<li>We may terminate our relationship with other individuals and organisations working on our behalf if they breach this policy.</li>
+</ul>
+<p><strong>How you can contact us</strong></p>
+<p>This Modern Slavery, Human Trafficking and Child Labour policy is reviewed annually unless there is a requirement to review it more frequently.</p>
+<p>Please contact us on&nbsp;<a href="mailto:people@rpint.com"><strong>people@rpint.com</strong></a>&nbsp;if you have any further questions.</p>
+      </section>
       </LegalBody>
     </Layout>
   );
